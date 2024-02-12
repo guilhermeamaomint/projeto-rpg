46c7c47740473e0ed6d13310ccced857c7dff2d5
guilhermegit <g360guilherme@gmail.com>
2024-02-12 05:30:14 -0300

    mais alterações

--- projeto_rpg/coiso_la.py
+++ projeto_rpg/coiso_la.py
@@ -2,7 +2,7 @@
 # bibliotecas #
 ###############
 
-import random as rd
+import funcoes
 
 ############
 # verdades #
@@ -36,35 +36,6 @@ fichas = ["darth", "skamos", "5"]
 modificador2 = 0
 total = 0
 totaltotal = 0
-
-###########
-# funções #
-###########
-
-def lancardados():
-    dados = {
-        "d4": 4,
-        "d6": 6,
-        "d8": 8,
-        "d10": 10,
-        "d12": 12,
-        "d20": 20
-    }
-    total = 0
-    nmr_de_dados = int(input("Digite o número de dados que você quer lançar: "))
-    tipo = input("Digite o tipo de dado que deseja lançar (d4, d6, d8, d10, d12, d20): ")
-    
-    if tipo in dados:
-        lados = dados[tipo]
-        while nmr_de_dados >= 1:
-            d = rd.randint(1, lados)
-            total = total + d
-            nmr_de_dados = nmr_de_dados - 1
-            print(f"total {total}")
-    else:
-        print("Tipo de dado inválido.")
-
-    return total
 
 ####################
 # inicio do codigo #
